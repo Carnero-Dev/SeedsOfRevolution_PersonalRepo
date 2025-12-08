c90d83114ff716269a3b46f98ee5badd46128ed8
Lenovo-2024-carneroDev\Jukilo <carnero.dev@gmail.com>
2025-12-08 20:21:02 +0100

    feat(ProvinceData): Crear Data con info de provincias

--- SoR_Project/Assets/Scripts/Data/Models/GameData/GameData.cs
+++ SoR_Project/Assets/Scripts/Data/Models/GameData/GameData.cs
@@ -5,4 +5,5 @@ public class GameData : IData {
     public float version = 1.0f; // versión del GameData para auditoria
     public RunData run = new();
     public TimeManagerData timeManager = new();
+    public ProvinceData[] provinces = Array.Empty<ProvinceData>();
 }
---
+++ SoR_Project/Assets/Scripts/Data/Models/GameData/ProvinceData.cs
@@ -0,0 +1,8 @@
+public class ProvinceData {
+	public string provinceID;
+
+	public float currentPopularity;
+	public float currentAligned;
+	public float currentAffiliates;
+	public float stability;
+}
