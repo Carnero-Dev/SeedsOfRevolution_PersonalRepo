5db00333a3d3cf873ebb68a4e572501bf820d9f0
Lenovo-2024-carneroDev\Jukilo <carnero.dev@gmail.com>
2025-12-13 18:13:34 +0100

    fix(ProvinceUI) Suscribir al Update el método de actualización de interfaz y ajustar fuentes

--- SoR_Project/Assets/Scripts/components/GameProvinceParameter_ui.cs
+++ SoR_Project/Assets/Scripts/components/GameProvinceParameter_ui.cs
@@ -21,25 +21,26 @@ public class GameProvinceParameter_ui : MonoBehaviour {
         _provinceManager = ServiceLocator.Get<ProvinceManager>();
         _timeManager = ServiceLocator.Get<TimeManager>();
         
-        _timeManager.OnDayPassedEvent += UpdateUi;
-        _provinceManager.OnProvinceSelected += ProvinceInfoVisibility;
-        
+        _provinceManager.OnProvinceSelected += ProvinceInfoVisibility; 
+    }
+
+    private void Update() {
+        if(_provinceManager.selectedProvince == null) return;
+        UpdateUi();
     }
 
 	private void OnDisable() {
-		_timeManager.OnDayPassedEvent -= UpdateUi;
         _provinceManager.OnProvinceSelected -= ProvinceInfoVisibility;
 	}
-    //TODO: Arreglar que pille con un dia de retraso los valores si se cambia de provincia en el mismo dia de juego
 	private void UpdateUi() {
         if(_provinceManager.selectedProvince == null) return;
         provinceNameText.text = _provinceManager.selectedProvince.ProvinceName;
         //provinceFlagImage.sprite = _provinceManager.selectedProvince.flagImage;
         provinceStabilityText.text = "Estabilidad: " + _provinceManager.selectedProvince.stability.ToString("F2") + "%";
-        provincePopulationText.text = "Habitantes: " + _provinceManager.selectedProvince.Population.ToString("F2");
-        provincePopularityText.text = _provinceManager.selectedProvince.popularity.ToString("F2");
-        provinceAlignedText.text = _provinceManager.selectedProvince.aligned.ToString("F2");
-        provinceAffiliatesText.text = _provinceManager.selectedProvince.affiliates.ToString("F2");
+        provincePopulationText.text = "Habitantes: " + _provinceManager.selectedProvince.Population.ToString();
+        provincePopularityText.text = _provinceManager.selectedProvince.popularity.ToString();
+        provinceAlignedText.text = _provinceManager.selectedProvince.aligned.ToString();
+        provinceAffiliatesText.text = _provinceManager.selectedProvince.affiliates.ToString();
     }
 
     private void ProvinceInfoVisibility(bool show) {
